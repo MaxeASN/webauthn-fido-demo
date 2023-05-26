d38436845a26fa9f242b5b5b0c28e3fe84f5f635
Sining Chen <382724644@qq.com>
2023-05-26 16:14:58 +0800

    新增l1地址接口

--- src/main/java/com/mih/webauthn/demo/controller/L1Controller.java
+++ src/main/java/com/mih/webauthn/demo/controller/L1Controller.java
@@ -5,16 +5,21 @@ import com.mih.webauthn.demo.domain.L1Address;
 import com.mih.webauthn.demo.domain.L1AddressRepo;
 import com.mih.webauthn.demo.domain.Wallet;
 import com.mih.webauthn.demo.domain.WalletRepo;
+import com.mih.webauthn.demo.domain.dto.RegisterL1Params;
+import com.mih.webauthn.demo.domain.dto.RegisterL1Response;
+import com.mih.webauthn.demo.service.L1AddressService;
+import com.mih.webauthn.demo.service.L1RpcService;
 import io.github.webauthn.domain.WebAuthnUserRepository;
 import io.github.webauthn.jpa.JpaWebAuthnUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,6 +33,16 @@ public class L1Controller {
     @Autowired
     private WebAuthnUserRepository<JpaWebAuthnUser> webAuthnUserRepository;
 
+    @Autowired
+    private L1AddressService l1AddressService;
+
+    @Autowired
+    private WalletRepo walletRepo;
+
+    @Autowired
+    @Qualifier("l1RpcService")
+    private L1RpcService l1RpcService;
+
     @RequestMapping("list")
     public CommonResult queryL1Address(@AuthenticationPrincipal UserDetails user){
         if(user == null){
@@ -38,5 +53,26 @@ public class L1Controller {
         List<L1Address> entityList = l1AddressRepo.findAllByAppUserId(userEntity.getId());
         List<String> l1AddressList = entityList.stream().map(L1Address -> L1Address.getL1Address().toLowerCase()).collect(Collectors.toList());
         return CommonResult.success(l1AddressList);
+    }
+
+    @RequestMapping("add")
+    public CommonResult addL1Address(@AuthenticationPrincipal UserDetails user){
+        if(user == null){
+            return CommonResult.failed("用户尚未登录");
+        }
+        JpaWebAuthnUser userEntity = webAuthnUserRepository.findByUsername(user.getUsername()).get();
+        Wallet wallet = walletRepo.findByAppUserId(userEntity.getId()).iterator().next();
+        RegisterL1Response response = null;
+        //TODO:用熔断器替代try catch
+        try {
+            response = l1RpcService.maxeRegistL1(new RegisterL1Params(1, wallet.getContractAddress()));
+        } catch (Exception e) {
+            return CommonResult.failed("调用rpc接口生成l1 address出错");
+        }
+        if(response == null || response.getError() != null){
+            return CommonResult.failed("调用rpc接口生成l1 address出错");
+        }
+        l1AddressService.addL1Address(userEntity.getId(), response.getResult().getL1Address());
+        return CommonResult.success();
     }
 }
---
+++ src/main/java/com/mih/webauthn/demo/service/L1AddressService.java
@@ -0,0 +1,5 @@
+package com.mih.webauthn.demo.service;
+
+public interface L1AddressService {
+    void addL1Address(Long appUserId, String l1Address);
+}
